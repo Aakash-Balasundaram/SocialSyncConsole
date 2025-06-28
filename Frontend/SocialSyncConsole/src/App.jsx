16b4c2a955b817477267afcf740f47fd450a562c
S-Bharath16 <bharathshan@outlook.com>
2025-06-29 03:04:11 +0530

    Home page, Dashboard, Footer included

---
+++ Frontend/SocialSyncConsole/src/App.jsx
@@ -0,0 +1,14 @@
+import './App.css'
+import Dashboard from './Components/Dashboard'
+import Footer from './Components/Footer'
+
+function App() {
+  return(
+    <div className="App">
+      <Dashboard />
+      <Footer />
+    </div>
+  )
+}
+
+export default App
---
+++ Frontend/SocialSyncConsole/src/Components/Footer.jsx
@@ -0,0 +1,21 @@
+function Footer() {
+    const footerStyle = {
+        backgroundColor: '#2C3E50',
+        textAlign: 'center',
+        fontFamily: 'Arial, sans-serif',
+        color: '#ffffff',
+        padding: '1rem',
+        position: 'fixed',
+        bottom: '0',
+        left: '0',
+        width: '100%',
+        zIndex: '1000'
+    };
+    return(
+        <footer style={footerStyle}>
+            <p style={{margin: '0'}}>SocialSyncConsole Version 0.1</p>
+            </footer>
+    );
+}
+
+export default Footer;
