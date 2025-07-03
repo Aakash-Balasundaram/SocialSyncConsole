770e6e6afba68e868bf594acd7221b167e709f89
Aakash-Balasundaram <aakash735cse@gmail.com>
2025-07-03 10:36:28 +0530

    Tailwind CSS classes for Navbar component

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,12 +1,12 @@
 import './App.css'
 import Dashboard from './Components/Dashboard'
 import Footer from './Components/Footer'
-// import Navbar from './Components/Navbar'
+import Navbar from './Components/Navbar'
 
 function App() {
   return (
     <div >
-      {/* <Navbar /> */}
+      <Navbar />
       <main className="pt-16"> {/* pt-16 ≈ padding-top: 4rem to push below navbar */}
         <Dashboard />
       </main>
--- Frontend/src/Components/Navbar.jsx
+++ Frontend/src/Components/Navbar.jsx
@@ -3,18 +3,16 @@ import { MdNotifications } from "react-icons/md";
 
 export default function Navbar() {
   return (
-    <nav className="fixed top-0 left-0 w-full z-50 bg-transparent px-6 py-2 flex items-center justify-between">
+    <nav className="fixed top-0 left-0 w-full z-50 bg-transparent px-6 py-2 items-center justify-between">
       {/* Left: Logo */}
-      <div className="flex items-center">
         <img
           src="/src/assets/SYCicon.svg" // Make sure this path is correct relative to public or src
           alt="SYC Logo"
           className="h-8 w-auto object-contain" // h-8 for smaller height, auto width
         />
-      </div>
 
       {/* Right: Icons */}
-      <div className="flex items-center gap-4">
+      <div className="flex items-center gap-4 flex flex-col">
         <CgProfile size={24} className="text-gray-300 cursor-pointer hover:text-white transition" />
         <MdNotifications size={24} className="text-gray-300 cursor-pointer hover:text-white transition" />
       </div>
